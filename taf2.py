775a68d506d59d6ceecc6e29ddb975a787deb85d
Tim Pillinger <tim.pillinger@metoffice.gov.uk>
2017-03-31 15:18:10 +0100

    further refactoring complete. Class for taf now starting to look ok

--- taf2.py
+++ taf2.py
@@ -4,7 +4,7 @@ TAF Programme designed to return a graphical interpretation
 @TODO: Graphical Output
        Create Class for each change group
 '''
-
+import datetime as dt
 EXAMPLE = '''EGXE 161400Z 1615/1702 22015KT 9999 FEW020 SCT025 TEMPO 1622/1702 24022G32KT SCT020 BECMG 1615/1619 5000 -RA 30010KT'''
 SPLITS = ['BECMG', 'TEMPO', 'PROB40', 'PROB30']
 ALLOWED_WX = ['-RA']
@@ -18,7 +18,10 @@ class Taf():
         self.groups_proc = self.process_groups()
 
     def __str__(self):
-        return str(self.groups_proc)
+        a = self.groups_raw
+        b = self.groups_proc        
+        msg = "\nGroups raw is:\n{}\nGroups proc is:\n{}".format(a,b)
+        return msg
 
     def get_taf_as_dict(self):
         '''
@@ -46,9 +49,101 @@ class Taf():
                 out = self.process_group()
             return out
 
-    def process_group(self):
-        out = {'clouds': [], 'wx': [], 'duration': 0, 'vis':None, 'vis_colour':{'colour': 'NIL', 'hex': '#00000000'}}        
 
+    def get_cloud_group_dict(self, clouds, mil_rules=True):
+        '''
+        Returns a list of dictionaries, one for each cloud
+        group and one for the lowest cloud height
+        '''
+        if mil_rules is True:
+            sig_cloud = ['SCT', 'BKN', 'OVC']
+        elif mil_rules is False:
+            sig_cloud = ['BKN', 'OVC']
+        out = []
+        for cloud in clouds:
+            detail = {'height': int(cloud[3:]) * 100, 'amt': cloud[:3]}
+            out.append(detail)
+        lowest_base = 10000
+        for cloud in out:
+            if cloud['height'] < lowest_base and cloud['amt'] in sig_cloud:
+                lowest_base = cloud['height']
+        out.append({'lowest_base': lowest_base})
+        return out
+
+    def cloud_analysis(self, clouds):
+        '''
+        takes a list of clouds in string form and returns dicts
+        '''
+        if len(clouds) == 0:
+            return ['nsc']
+        else:
+            clouds = self.get_cloud_group_dict(clouds)
+            return clouds
+
+    def vis_colour(self, vis):
+        if vis > 7999:
+            vis_colour = {'colour': 'BLU', 'hex':'#0000EE'}
+        elif vis > 4999:
+            vis_colour = {'colour': 'WHT', 'hex':'#EEEEEE'}
+        elif vis > 3699:
+            vis_colour = {'colour': 'GRN', 'hex':'#00EE00'}
+        elif vis > 2499:
+            vis_colour = {'colour': 'YLO1', 'hex':'#00EEEE'}
+        elif vis > 1599:
+            vis_colour = {'colour': 'YLO2', 'hex':'#00AAAA'}
+        elif vis > 799:
+            vis_colour = {'colour': 'AMB', 'hex':'#CC8888'}
+        elif vis < 800:
+            vis_colour = {'colour': 'RED', 'hex':'#EE0000'}
+        else:
+            vis_colour = {'colour': 'ERR', 'hex':'#000000'}
+        return vis_colour
+
+
+    def dict_tafgroup(self, tafgroup):
+        '''
+        takes taf data and turns it into a dict.
+        '''
+        out = {'clouds': [], 'wx': [], 'duration': 0, 'vis':None, 'vis_colour':{'colour': 'NIL', 'hex': '#00000000'}}
+        clouds = []
+        for word in tafgroup:
+            if '/' in word:
+                out['time_group'] = word
+                out['hour_start'] = word.split('/')[0][2:]
+                out['hour_end'] = word.split('/')[1][2:]
+                out['day_start'] = word.split('/')[0][:2]
+                out['day_end'] = word.split('/')[1][:2]
+                end_time = 24 * int(out['day_end']) + int(out['hour_end'])
+                start_time = 24 * int(out['day_start']) + int(out['hour_start'])
+                out['duration'] = end_time - start_time
+            elif word in SPLITS:
+                out['change_type'] = word
+            elif len(word) == 4 and isinstance(int(word), int):
+                out['vis'] = int(word)
+                out['vis_colour'] = self.vis_colour(int(word))
+            elif word[-2:] == "KT":
+                out['wind'] = word
+                out['max_wind'] = word[-4:-2]
+            elif len(word) == 6:
+                clouds.append(word)
+            elif word in ALLOWED_WX:
+                out['wx'].append(word)
+        out['clouds'] = self.cloud_analysis(clouds)
+        out['start_time_since_base'] = "Dave"
+
+        return out
+
+    def process_group(self):
+        out = {}
+        # out = {'clouds': [], 'wx': [], 'duration': 0, 'vis':None, 'vis_colour':{'colour': 'NIL', 'hex': '#00000000'}}        
+        for label, tafgroup in self.groups_raw.iteritems():
+            if label == 'base_time':
+                out['base_time'] = tafgroup
+            elif label == 0:
+                out['ICAO'] = tafgroup[0]
+                out['base_group'] = self.dict_tafgroup(tafgroup[3:])
+            else:
+                out[label] = self.dict_tafgroup(tafgroup)
         return out
 
 
